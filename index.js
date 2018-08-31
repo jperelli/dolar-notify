22e4867d26bb6d63c47f70ecc4af3011bbca7add
Julian Perelli <jperelli@gmail.com>
2018-08-31 11:50:10 -0300

    catch http errors

--- index.js
+++ index.js
@@ -5,25 +5,35 @@ const cheerio = require('cheerio');
 let PRECIO = 0
 
 function notify_precio() {
-  axios.get('http://www.bna.com.ar/Personas').then(response => {
-    let $ = cheerio.load(response.data);
-    let precio = $('.table.cotizacion tbody tr').first().find('td').last().text()
-    precio = parseFloat(precio.split(',').join('.'))
-    let str = `${PRECIO ? 'ANTERIOR: ' + PRECIO + '\n' : ''}ACTUAL: ${precio}`
+  axios({
+    url: 'http://www.bna.com.ar/Personas',
+    timeout: 60000,
+  })
+  .catch(e => {
+    console.log('error: ', e.toString())
+    setTimeout(notify_precio, 1000)
+  })
+  .then(response => {
+    if (response) {
+      let $ = cheerio.load(response.data);
+      let precio = $('.table.cotizacion tbody tr').first().find('td').last().text()
+      precio = parseFloat(precio.split(',').join('.'))
+      let str = `${PRECIO ? 'ANTERIOR: ' + PRECIO + '\n' : ''}ACTUAL: ${precio}`
 
-    if (precio !== PRECIO) {
-      console.log(str)
-      PRECIO = precio
+      if (precio !== PRECIO) {
+        console.log(str)
+        PRECIO = precio
 
-      notifier.notify({
-        title: 'Precio Dolar',
-        message: str,
-        wait: true,
-        closeLabel: 'cerrar',
-        time: 2000000
-      });
+        notifier.notify({
+          title: 'Precio Dolar',
+          message: str,
+          wait: true,
+          closeLabel: 'cerrar',
+          time: 2000000
+        });
+      }
+      setTimeout(notify_precio, 10000)
     }
-    setTimeout(notify_precio, 10000)
   });
 }
 notify_precio()
